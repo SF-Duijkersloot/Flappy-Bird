198d8d4c6591cf56c2a90f477d34c650ee6fa2df
Sep Duijkersloot <43574905+SF-Duijkersloot@users.noreply.github.com>
2023-02-16 17:21:25 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-
 var bird;
 var pipes = [];
+var score = 0;
 
 function setup() {
   createCanvas(640, 480);
@@ -11,21 +11,27 @@ function setup() {
 function draw() {
   background(0);
 
-  for (var i = pipes.length-1; i >= 0; i--) {
+  for (var i = pipes.length - 1; i >= 0; i--) {
     pipes[i].show();
     pipes[i].update();
 
     if (pipes[i].hits(bird)) {
       console.log("HIT");
+      score = 0;
     }
 
     if (pipes[i].offscreen()) {
       pipes.splice(i, 1);
+      score++;
     }
   }
 
   bird.update();
   bird.show();
+
+  fill(255);
+  textSize(32);
+  text("Score: " + score, 10, 30);
 
   if (frameCount % 75 == 0) {
     pipes.push(new Pipe());
@@ -33,8 +39,7 @@ function draw() {
 }
 
 function keyPressed() {
-  if (key == ' ') {
+  if (key == " ") {
     bird.up();
-    console.log("SPACE");
   }
 }
