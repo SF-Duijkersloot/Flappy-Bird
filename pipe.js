90f3f4baee25ae0c491c74c36ca35771f2acf99b
Sep Duijkersloot <43574905+SF-Duijkersloot@users.noreply.github.com>
2023-02-16 17:00:43 +0100

    Update pipe.js

--- pipe.js
+++ pipe.js
@@ -1,45 +1,45 @@
+
 function Pipe() {
-    this.spacing = 175;
-    this.top = Math.random() * (height / 2);
-    this.bottom = height - (this.top + this.spacing);
-    this.x = width;
-    this.w = 80;
-    this.speed = 6;
-  
-    this.highlight = false;
-  
-    this.hits = function(bird) {
-      if (bird.y < this.top || bird.y > height - this.bottom) {
-        if (bird.x > this.x && bird.x < this.x + this.w) {
-          this.highlight = true;
-          return true;
-        }
+  this.spacing = 175;
+  this.top = random(height / 6, 3 / 4 * height);
+  this.bottom = height - (this.top + this.spacing);
+  this.x = width;
+  this.w = 80;
+  this.speed = 6;
+
+  this.highlight = false;
+
+  this.hits = function(bird) {
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {
+        this.highlight = true;
+        return true;
       }
-      this.highlight = false;
+    }
+    this.highlight = false;
+    return false;
+  }
+
+  this.show = function() {
+    fill(255);
+    if (this.highlight) {
+      fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+  }
+
+  this.update = function() {
+    this.x -= this.speed;
+  }
+
+  this.offscreen = function() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
       return false;
     }
-  
-    this.show = function() {
-      var canvas = document.getElementById("canvas");
-      var ctx = canvas.getContext("2d");
-      ctx.fillStyle = "white";
-      if (this.highlight) {
-        ctx.fillStyle = "red";
-      }
-      ctx.fillRect(this.x, 0, this.w, this.top);
-      ctx.fillRect(this.x, height - this.bottom, this.w, this.bottom);
-    }
-  
-    this.update = function() {
-      this.x -= this.speed;
-    }
-  
-    this.offscreen = function() {
-      if (this.x < -this.w) {
-        return true;
-      } else {
-        return false;
-      }
-    }
   }
-  
+
+
+}
